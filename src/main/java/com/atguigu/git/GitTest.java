b2c0050ef3391645fb0316052345294ebeb30871
ITSky <912061242@qq.com>
2022-07-07 17:01:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 912061242@qq.com
+ * @create 2022-07-07 16:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
